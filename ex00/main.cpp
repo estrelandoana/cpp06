ef9a05df9e55dead6bf219f80033c89ac14cc4d6
estrelandoana <lana.ohama@gmail.com>
2025-10-26 15:59:09 -0300

    NewUpdate

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: apaula-l <apaula-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 02:00:46 by apaula-l          #+#    #+#             */
-/*   Updated: 2025/10/21 21:05:01 by apaula-l         ###   ########.fr       */
+/*   Updated: 2025/10/26 15:55:37 by apaula-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 int main(int argc, char** argv) {
     if (argc != 2) {
-        std::cout << "Usage: ./convert <literal>" << std::endl;
+        std::cout << "Usage: ./ScalarConverter <literal>" << std::endl;
         return 1;
     }
     ScalarConverter::convert(argv[1]);
